e975610a58f8ac16b7545e588ee1b857641af2d4
DJCAYZ <charlesaaron07@gmail.com>
2024-05-31 01:33:34 +0800

    Update PositionallyShiftedCipher.java
    A much more elegant solution by Betorio. Thanks!

--- src/PositionallyShiftedCipher.java
+++ src/PositionallyShiftedCipher.java
@@ -1,79 +1,50 @@
-public class PositionallyShiftedCipher {
-    public String encrypt(String plaintext) {
-        // Trim whitespace at the start and end of the string
-        // then, convert it to all lowercase
-        String cleanPlaintext = plaintext.trim().toLowerCase();
-        String encryptedText = ""; // String to hold the encrypted text
+import java.util.Scanner;
 
-        // Iterate through every character in the plaintext string
+public class PositionallyShiftedCipher {
+
+    public String encrypt(String plaintext) {
+        String cleanPlaintext = plaintext.trim().toLowerCase();
+        StringBuilder encryptedText = new StringBuilder();
+
         for (int i = 0; i < cleanPlaintext.length(); i++) {
-            // Get the current character being iterated
             char currentLetter = cleanPlaintext.charAt(i);
 
-            // if the character is not in the range a-z
             if (currentLetter < 'a' || currentLetter > 'z') {
-                // just add it as is, then continue to the next iteration
-                encryptedText += currentLetter;
+                encryptedText.append(currentLetter);
                 continue;
             }
 
-            // else, shift the letter using the getShiftedLetter() method
-            encryptedText += getShiftedLetter(currentLetter, i+1);
+            encryptedText.append(getShiftedLetter(currentLetter, i + 1));
         }
 
-        // Once every character is iterated through, return the encrypted text result
-        return encryptedText;
-    }
-
-    private char getShiftedLetter(char letter, int position) {
-        // we first subtract 96 to the letter to get its numerical value
-        // from 1 to 26
-        // then we add the position of it in the string (not the index, so it starts with 1)
-        int shifted = (letter - 96) + (position % 26);
-
-        // if the shifted value is greater than 26
-        if (shifted > 26) {
-            // subtract 26 to loop back around to the start of the alphabet
-            shifted -= 26;
-        }
-
-        // return the shifted value as a char by first adding 96 to it to make it a valid ASCII value
-        // then type cast it to a char
-        return (char) (shifted + 96);
+        return encryptedText.toString();
     }
 
     public String decrypt(String encryptedText) {
-        // Same thing as the encrypt() method
         String cleanEncryptedText = encryptedText.trim().toLowerCase();
-        String plaintext = "";
+        StringBuilder plaintext = new StringBuilder();
 
         for (int i = 0; i < cleanEncryptedText.length(); i++) {
             char currentLetter = cleanEncryptedText.charAt(i);
 
             if (currentLetter < 'a' || currentLetter > 'z') {
-                plaintext += currentLetter;
+                plaintext.append(currentLetter);
                 continue;
             }
 
-            // We instead put the letter into the getUnshiftedLetter() method
-            plaintext += getUnshiftedLetter(currentLetter,i + 1);
+            plaintext.append(getUnshiftedLetter(currentLetter, i + 1));
         }
 
-        return plaintext;
+        return plaintext.toString();
+    }
+
+    private char getShiftedLetter(char letter, int position) {
+        int shifted = ((letter - 'a' + position) % 26 + 26) % 26 + 'a';
+        return (char) shifted;
     }
 
     private char getUnshiftedLetter(char letter, int position) {
-        // same thing as in getShiftedLetter, but we instead subtract the position instead of adding it
-        int unshifted = (letter - 96) - (position % 26);
-
-        // if the unshifted value is less than 1
-        if (unshifted < 1) {
-            // add 26 so it goes back to the end of the alphabet
-            unshifted += 26;
-        }
-
-        // add 96 to make it a valid ASCII value then type cast it to a char
-        return (char) (unshifted + 96);
+        int unshifted = ((letter - 'a' - position) % 26 + 26) % 26 + 'a';
+        return (char) unshifted;
     }
-
 }
